0a7f926b3e44627e8e47c5ca3db76d24a0bf9c1c
brandonjolley97 <bjol7457@740s-pl-131.local>
2015-10-21 08:31:41 -0600

    Add to Repo

---
+++ src/chat/controller/ChatRunner.java
@@ -0,0 +1,17 @@
+package chat.controller;
+
+/**
+ * Starts chatbot program
+ * @author Brandon Jolley
+ * @version 1.0 10/21/15
+ */
+
+public class ChatRunner
+{
+	public static void main(String [] args)
+	{
+		ChatbotController myChatbot = new ChatbotController();
+		myChatbot.start();
+		
+	}
+}
---
+++ src/chat/controller/ChatbotController.java
@@ -0,0 +1,9 @@
+package chat.controller;
+
+public class ChatbotController
+{
+	start()
+	{
+		
+	}
+}
