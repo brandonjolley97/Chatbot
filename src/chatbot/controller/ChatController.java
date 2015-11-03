7e292dae0cc839169c329d7fbb010e8b09334df8
brandonjolley97 <brandonjolley97@gmail.com>
2015-11-03 09:40:05 -0700

    completed content checker

--- src/chatbot/controller/ChatController.java
+++ src/chatbot/controller/ChatController.java
@@ -2,7 +2,6 @@ package chatbot.controller;
 
 import chatbot.view.ChatView;
 import chatbot.model.ChatModel;
-
 /**
  * Application Controller for the Chatbot Project
  * @author bjol7457
@@ -17,45 +16,26 @@ public class ChatController
 	public ChatController()
 	{
 		display = new ChatView();
-		String user = display.collectUserText("What is your name?");
-		simpleBot =  new Chatbot(user);
+		String user = display.grabInput("What is your name?");
+		simpleBot =  new ChatModel(user);
 	}
 	
 	
 	public void start()
 	{
-		display.displayText("Hello " + simpleBot.getUserName());
+		display.showResponse("Hello " + simpleBot.getUserName());
 		chat();
 	}
 	
 	public void chat()
 	{
-		String conversation = display.collectUserText("What would you like to talk about today?");
+		String conversation = display.grabInput("What would you like to talk about today?");
 		while(simpleBot.lengthChecker(conversation))
 		{
-			if(simpleBot.contentChecker(conversation))
-			{
-				display.displayText("Wow, I had no idea you loved " + simpleBot.getContent());
-			}
-			else if(simpleBot.memeChecker(conversation))
-			{
-				display.displayText("what a lame meme :(");
-			}
-			
-			conversation = display.collectUserText(conversation);
+			conversation = simpleBot.processConversation(conversation);
+			conversation = display.grabInput(conversation);
 		}
 	}
-	
-	private void shutDown()
-	{
-		
-		display.displayText("Goodbye")
-
-		
-		
-		
-		
-	} 
 	
 	
 	
