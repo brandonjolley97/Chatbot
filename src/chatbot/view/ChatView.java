ab84d19c5a3b1abde909cd1ee010548d0333ea4e
brandonjolley97 <brandonjolley97@gmail.com>
2015-11-05 08:43:40 -0700

    Added stuff for the icon and popups

--- src/chatbot/view/ChatView.java
+++ src/chatbot/view/ChatView.java
@@ -1,18 +1,32 @@
 package chatbot.view;
 
 import javax.swing.JOptionPane;
+import javax.swing.ImageIcon;
 
 public class ChatView
 {
-	public void showResponse(String otherChatWords)
+	private String windowMessage;
+	private ImageIcon chatIcon;
+	
+	public ChatView()
 	{
-		JOptionPane.showMessageDialog(null, otherChatWords);
+		windowMessage = "I am definitelt not a robot! Hello!";
+		chatIcon = new ImageIcon(getClass().getResource("images/porky.png"));
+		
+		
 	}
 	
 	public String grabInput(String displayText)
 	{
 		String returnText = "";
-		returnText = JOptionPane.showInputDialog(null, displayText);
+		returnText = JOptionPane.showInputDialog(null, displayText, windowMessage, JOptionPane.PLAIN_MESSAGE, chatIcon, null, "Answer here").toString();
 		return returnText;
 	}
+	
+	public void showResponse(String otherChatWords)
+	{
+		JOptionPane.showMessageDialog(null, otherChatWords);
+	}
+	
+	
 }
