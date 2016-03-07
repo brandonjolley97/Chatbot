0ebaae0bf13cfe86ae775ed5a0923c0200e84036
brandonjolley97 <brandonjolley97@gmail.com>
2016-03-07 09:15:11 -0700

    Added a sendTweet method and created a Twitter object

--- src/chatbot/model/CTECTwitter.java
+++ src/chatbot/model/CTECTwitter.java
@@ -11,6 +11,20 @@ import java.util.ArrayList;
 
 public class CTECTwitter 
 {
-	private ArrayList<String> statusList;
+	private ArrayList<Status> statusList;
 	private ArrayList<String> wordList;
+	private Twitter chatbotTwitter;
+	
+	public CTECTwitter()
+	{
+		statusList = new ArrayList<Status>();
+		wordList = new ArrayList<String>();
+	}
+	
+	public void sendTweet()
+	{
+		String tweet;
+		
+		chatbotTwitter.updateStatus("I just tweeted from my Java Chatbot program! #APCSRocks @CTECNow Thanks @cscheerleader & @codyhenrichsen!");
+	}
 }
