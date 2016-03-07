1eb07e6e8b23eda1b8552637b7599b651a7eedc1
brandonjolley97 <brandonjolley97@gmail.com>
2016-03-07 08:59:39 -0700

    Added imports and declared arrays

--- src/chatbot/model/CTECTwitter.java
+++ src/chatbot/model/CTECTwitter.java
@@ -1,11 +1,16 @@
 package chatbot.model;
+
 /**
- * @version 0.x
+ * @version 0.1
  * @author brandon jolley
  *
  */
 
+import twitter4j.*;
+import java.util.ArrayList;
+
 public class CTECTwitter 
 {
-
+	private ArrayList<String> statusList;
+	private ArrayList<String> wordList;
 }
